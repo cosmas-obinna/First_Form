daa44cc2d7c2e80ef0de6a6d0977e3587d18edee
cosmas-obinna <88593083+cosmas-obinna@users.noreply.github.com>
2021-09-07 13:29:51 +0100

    default-date

--- include/header.php
+++ include/header.php
@@ -8,8 +8,9 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="css/site.css">
     <div class="container">
-    <title>MyApp - <?php echo $title ?></title>
+        <title>MyApp - <?php echo $title ?></title>
   </head>
   <body>
