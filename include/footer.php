ccf328824aa340c8241d2d36a7b74bc99633316c
cosmas-obinna <88593083+cosmas-obinna@users.noreply.github.com>
2021-09-07 13:29:42 +0100

    default-date

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,8 @@
 
-<div>
-   <?php 
-   echo 'copyright ' . date('y') ?>
+    <div id="footer">
+      <?php 
+      echo 'copyright ' . date('y') ?>
+    </div>
 </div>
 
 
@@ -16,5 +17,12 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
     -->
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script>
+        $( function() {
+          $( "#dob" ).datepicker();
+        } );
+    </script>
   </body>
-</html>
+</html> 
